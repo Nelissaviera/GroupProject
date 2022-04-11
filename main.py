1c4a6c61d041fb3a9ab864b855320ac29b2cd575
zuperhenry <henry@web-master.co>
2022-04-11 19:51:35 -0400

    Update main.py

--- main.py
+++ main.py
@@ -15,109 +15,109 @@ def mainMenu():
         Salt = 10.00
         Water = 275.00
         print(f' Bread recipe for Multigrain for {quantity} is:')
-        print('Total Flour:', TotalFlour * quantity)
-        print('RedWheat:', RedWheat * quantity)
-        print('WhiteWheat:', WhiteWheat * quantity)
-        print('Rye:', Rye * quantity)
-        print('Bread Flour', BreadFlour * quantity)
-        print('Salt:', Salt * quantity)
-        print('Water:', Water * quantity)
+        print(f'Total Flour: {TotalFlour * quantity}g.')
+        print(f'RedWheat: {RedWheat * quantity}g.')
+        print(f'WhiteWheat: {WhiteWheat * quantity}g.')
+        print(f'Rye: {Rye * quantity}g.')
+        print(f'Bread Flour {BreadFlour * quantity}g.')
+        print(f'Salt: {Salt * quantity}g.')
+        print(f'Water: {Water * quantity}ml.')
     elif(recipeOption == 2):
-        TotalFlour = 60.00
-        RedWheat = 100.00
-        WhiteWheat = 20.00
+        TotalFlour = 50.00
+        RedWheat = 125.00
+        WhiteWheat = 25.00
         Rye = 5.00
-        BreadFlour = 200.00
-        Salt = 11.00
-        Water = 275.00
+        BreadFlour = 180.00
+        Salt = 10.00
+        Water = 250.00
         print(f' Bread recipe for Baguette for {quantity} is:')
-        print('Total Flour:', TotalFlour * quantity)
-        print('RedWheat:', RedWheat * quantity)
-        print('WhiteWheat:', WhiteWheat * quantity)
-        print('Rye:', Rye * quantity)
-        print('Bread Flour:', BreadFlour * quantity)
-        print('Salt:', Salt * quantity)
-        print('Water:', Water * quantity)
+        print(f'Total Flour: {TotalFlour * quantity}g.')
+        print(f'RedWheat: {RedWheat * quantity}g.')
+        print(f'WhiteWheat: {WhiteWheat * quantity}g.')
+        print(f'Rye: {Rye * quantity}g.')
+        print(f'Bread Flour {BreadFlour * quantity}g.')
+        print(f'Salt: {Salt * quantity}g.')
+        print(f'Water: {Water * quantity}ml.')
     elif(recipeOption == 3):
-        TotalFlour = 60.00
-        RedWheat = 100.00
+        TotalFlour = 75.00
+        RedWheat = 80.00
         WhiteWheat = 20.00
-        Rye = 5.00
-        BreadFlour = 200.00
-        Salt = 11.00
-        Water = 275.00
+        Rye = 8.00
+        BreadFlour = 190.00
+        Salt = 8.00
+        Water = 250.00
         print(f' Bread recipe for Bagel for {quantity} is:')
-        print('Total Flour:', TotalFlour * quantity)
-        print('RedWheat:', RedWheat * quantity)
-        print('WhiteWheat:', WhiteWheat * quantity)
-        print('Rye:', Rye * quantity)
-        print('Bread Flour:', BreadFlour * quantity)
-        print('Salt:', Salt * quantity)
-        print('Water:', Water * quantity)
+        print(f'Total Flour: {TotalFlour * quantity}g.')
+        print(f'RedWheat: {RedWheat * quantity}g.')
+        print(f'WhiteWheat: {WhiteWheat * quantity}g.')
+        print(f'Rye: {Rye * quantity}g.')
+        print(f'Bread Flour {BreadFlour * quantity}g.')
+        print(f'Salt: {Salt * quantity}g.')
+        print(f'Water: {Water * quantity}ml.')
     elif(recipeOption == 4):
-        TotalFlour = 60.00
-        RedWheat = 100.00
-        WhiteWheat = 20.00
-        Rye = 5.00
-        BreadFlour = 200.00
-        Salt = 11.00
-        Water = 275.00
+        TotalFlour = 40.00
+        RedWheat = 125.00
+        WhiteWheat = 25.00
+        Rye = 10.00
+        BreadFlour = 250.00
+        Salt = 5.00
+        Water = 225.00
         print(f' Bread recipe for Banana bread for {quantity} is:')
-        print('Total Flour:', TotalFlour * quantity)
-        print('RedWheat:', RedWheat * quantity)
-        print('WhiteWheat:', WhiteWheat * quantity)
-        print('Rye:', Rye * quantity)
-        print('Bread Flour:', BreadFlour * quantity)
-        print('Salt:', Salt * quantity)
-        print('Water:', Water * quantity)
+        print(f'Total Flour: {TotalFlour * quantity}g.')
+        print(f'RedWheat: {RedWheat * quantity}g.')
+        print(f'WhiteWheat: {WhiteWheat * quantity}g.')
+        print(f'Rye: {Rye * quantity}g.')
+        print(f'Bread Flour {BreadFlour * quantity}g.')
+        print(f'Salt: {Salt * quantity}g.')
+        print(f'Water: {Water * quantity}ml.')
     elif(recipeOption == 5):
-        TotalFlour = 60.00
-        RedWheat = 100.00
-        WhiteWheat = 20.00
-        Rye = 5.00
-        BreadFlour = 200.00
-        Salt = 11.00
-        Water = 275.00
+        TotalFlour = 75.00
+        RedWheat = 111.00
+        WhiteWheat = 22.00
+        Rye = 8.00
+        BreadFlour = 120.00
+        Salt = 7.00
+        Water = 225.00
         print(f' Bread recipe for Whole Wheat for {quantity} is:')
-        print('Total Flour:', TotalFlour * quantity)
-        print('RedWheat:', RedWheat * quantity)
-        print('WhiteWheat:', WhiteWheat * quantity)
-        print('Rye:', Rye * quantity)
-        print('Bread Flour:', BreadFlour * quantity)
-        print('Salt:', Salt * quantity)
-        print('Water:', Water * quantity)
+        print(f'Total Flour: {TotalFlour * quantity}g.')
+        print(f'RedWheat: {RedWheat * quantity}g.')
+        print(f'WhiteWheat: {WhiteWheat * quantity}g.')
+        print(f'Rye: {Rye * quantity}g.')
+        print(f'Bread Flour {BreadFlour * quantity}g.')
+        print(f'Salt: {Salt * quantity}g.')
+        print(f'Water: {Water * quantity}ml.')
     elif(recipeOption == 6):
-        TotalFlour = 60.00
-        RedWheat = 100.00
-        WhiteWheat = 20.00
-        Rye = 5.00
-        BreadFlour = 200.00
-        Salt = 11.00
-        Water = 275.00
+        TotalFlour = 90.00
+        RedWheat = 125.00
+        WhiteWheat = 15.00
+        Rye = 1.00
+        BreadFlour = 20.00
+        Salt = 10.00
+        Water = 175.00
         print(f' Bread recipe for Pita Bread for {quantity} is:')
-        print('Total Flour:', TotalFlour * quantity)
-        print('RedWheat:', RedWheat * quantity)
-        print('WhiteWheat:', WhiteWheat * quantity)
-        print('Rye:', Rye * quantity)
-        print('Bread Flour:', BreadFlour * quantity)
-        print('Salt:', Salt * quantity)
-        print('Water:', Water * quantity)
+        print(f'Total Flour: {TotalFlour * quantity}g.')
+        print(f'RedWheat: {RedWheat * quantity}g.')
+        print(f'WhiteWheat: {WhiteWheat * quantity}g.')
+        print(f'Rye: {Rye * quantity}g.')
+        print(f'Bread Flour {BreadFlour * quantity}g.')
+        print(f'Salt: {Salt * quantity}g.')
+        print(f'Water: {Water * quantity}ml.')
     elif(recipeOption == 7):
-        TotalFlour = 60.00
-        RedWheat = 100.00
-        WhiteWheat = 20.00
-        Rye = 5.00
+        TotalFlour = 80.00
+        RedWheat = 120.00
+        WhiteWheat = 10.00
+        Rye = 15.00
         BreadFlour = 200.00
-        Salt = 11.00
-        Water = 275.00
+        Salt = 4.00
+        Water = 75.00
         print(f' Bread recipe for Corn Bread for {quantity} is:')
-        print('Total Flour:', TotalFlour * quantity)
-        print('RedWheat:', RedWheat * quantity)
-        print('WhiteWheat:', WhiteWheat * quantity)
-        print('Rye:', Rye * quantity)
-        print('Bread Flour:', BreadFlour * quantity)
-        print('Salt:', Salt * quantity)
-        print('Water:', Water * quantity)
+        print(f'Total Flour: {TotalFlour * quantity}g.')
+        print(f'RedWheat: {RedWheat * quantity}g.')
+        print(f'WhiteWheat: {WhiteWheat * quantity}g.')
+        print(f'Rye: {Rye * quantity}g.')
+        print(f'Bread Flour {BreadFlour * quantity}g.')
+        print(f'Salt: {Salt * quantity}g.')
+        print(f'Water: {Water * quantity}ml.')
     else:
         print("Wrong choice, try again")
 
